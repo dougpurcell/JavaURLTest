123f72e76c89ab582fcd625b8eb06982d5a35433
dougpurcell <griffithsdl30@gmail.com>
2020-10-31 21:40:12 -0400

    charcounter working

--- src/URLTest.java
+++ src/URLTest.java
@@ -1,7 +1,65 @@
+import java.io.*;
+import java.net.*;
+
 public class URLTest {
 
+    private static String urlContents(String url) {
+        StringBuilder content = new StringBuilder();
+
+        try {
+            URL site = new URL(url);
+            URLConnection urlConnection = site.openConnection();
+            BufferedReader br = new BufferedReader(
+                    (new InputStreamReader(urlConnection.getInputStream()))
+            );
+            String line = br.readLine();
+
+            while (line != null) {
+                content.append(line + "\n");
+                br.close();
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return content.toString();
+
+    };
+
+    private static int urlCharCount (String url) {
+        int count = 0;
+
+        try {
+            URL site = new URL(url);
+            URLConnection urlConnection = site.openConnection();
+            BufferedReader br = new BufferedReader(
+                    (new InputStreamReader(urlConnection.getInputStream()))
+            );
+
+            String line = br.readLine();
+
+            for(int i = 0; i <line.length(); i++) {
+                if(line.charAt(0) != ' ') {
+                    count++;
+                }
+            }
+
+            br.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Shall we begin?");
+        int charCount = urlCharCount("https://cnn.com");
+        System.out.println(charCount);
+
+//        String lineCount = urlContents("https://cnn.com");
+//        System.out.println(lineCount);
 
     }
 }
